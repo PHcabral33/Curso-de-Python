e557855c6254dea6b0dbcab5733317da3d83493a
Paulo Henrique Cabral <phcloudgemini@gmail.com>
2025-05-22 15:26:40 -0300

    Adiciona VerificaIdade.py

---
+++ VerificaIdade.py
@@ -0,0 +1,10 @@
+def verifica_idade(idade):
+    if idade >18:
+        print('Você é de maior !')
+    else:
+        print('Você é de menor')
+    print(f'Você tem {idade} anos' )
+        
+    
+
+verifica_idade(33)
